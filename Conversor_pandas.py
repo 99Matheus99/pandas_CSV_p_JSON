3f0f6488f85a000b72c4ac4d4adfeda518acd2dd
matheus de oliveira <nubinhopro5@gmail.com>
2023-10-09 23:50:50 -0300

    melhora no código, melhora na visibilidade

--- Conversor_pandas.py
+++ Conversor_pandas.py
@@ -1,4 +1,3 @@
 import pandas as pd
-chaves = ['Nome', 'Idade', 'Cidade', 'Email', 'CPF',]
 df = pd.read_csv('dados.csv')
-df.to_json('novos_dados.json', orient='records', force_ascii=False)
+df.to_json('novos_dados.json', orient='records', force_ascii=False, indent=2)
