e9c15b45f7ad5c8acadf20b59dbafb37681bb07a
matheus de oliveira <nubinhopro5@gmail.com>
2023-10-10 19:34:34 -0300

    Tratando erro de não encontrar o arquivo

--- Conversor_pandas.py
+++ Conversor_pandas.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from time import sleep
-df = pd.read_csv('dados.csv') # abre o arquivo
-print('Arquivo dados.csv encontrado...')
-sleep(1) # espera 1 segundo
-print(f'Processando {len(df)} registros...')
-sleep(1)
-df.to_json('novos_dados.json', orient='records', force_ascii=False, indent=2) # converte de csv para json
-sleep(1)
-print('Arquivo novos_dados.json gerado!')
+try:
+    df = pd.read_csv('dados.csv') # abre o arquivo
+    print('Arquivo dados.csv encontrado...')
+    sleep(1) # espera 1 segundo
+    print(f'Processando {len(df)} registros...')
+    sleep(1)
+    df.to_json('novos_dados.json', orient='records', force_ascii=False, indent=2) # converte de csv para json
+    sleep(1)
+    print('Arquivo novos_dados.json gerado!')
+except FileNotFoundError:
+    print('Erro... Arquivo não encontrado!')
